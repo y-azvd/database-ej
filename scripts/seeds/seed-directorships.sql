1a194222748f4dcc62dd258c6d632572a3f75cc6
Edmundo Ribeiro <jtvedy@gmail.com>
2019-11-09 17:46:32 -0300

    Added two seed

---
+++ scripts/seeds/seed-directorships.sql
@@ -0,0 +1,12 @@
+DELETE FROM "directorships"; -- apagar todas as linha da tabela
+                       -- esse comando também reseta a contagem da coluna id
+
+INSERT INTO "directorships" (
+  "name"
+)
+VALUES 
+  ('Negócios'),
+  ('Operações'),
+  ('Marketing'),
+  ('Inovação')
+;
---
+++ scripts/seeds/seed-roles.sql
@@ -0,0 +1,13 @@
+DELETE FROM "roles"; -- apagar todas as linha da tabela
+                       -- esse comando também reseta a contagem da coluna id
+
+INSERT INTO "roles" (
+  "name"
+)
+VALUES 
+  ('Diretor'),
+  ('Consultor'),
+  ('Gerente de Projetos'),
+  ('Presidente Institucional'),
+  ('Presidente Organizacional')
+;
