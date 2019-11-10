da07ead8c3567ec3ddd5802ec2fa342088204a54
Yudi Yamane <yudiyamane@gmail.com>
2019-11-10 10:28:39 -0300

    altered members email from CHAR to VARCHAR

--- scripts/tables/create-members.sql
+++ scripts/tables/create-members.sql
@@ -4,7 +4,7 @@ CREATE TABLE "members"(
   "cpf" CHAR (11)  PRIMARY KEY NOT NULL,
   "registration" CHAR (10) UNIQUE NOT NULL,
   "name" VARCHAR (50) NOT NULL,
-  "email" CHAR (50) UNIQUE NOT NULL,
+  "email" VARCHAR (50) UNIQUE NOT NULL,
   "birth_date" DATE,
   "join_date" DATE,
   "exit_date" DATE,
