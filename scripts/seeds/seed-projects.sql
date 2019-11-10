6e6fdde037cf5d3424cf84beb7cc01ccdd254cb3
Yudi Yamane <yudiyamane@gmail.com>
2019-11-10 09:49:12 -0300

    add project_id to seeding

--- scripts/seeds/seed-projects.sql
+++ scripts/seeds/seed-projects.sql
@@ -1,8 +1,9 @@
 DELETE FROM "projects";
 
-INSERT INTO "projects"("name", "started_at", "delivery_at", "delivered_at", "link_drive", "difficulty", "revenue", "price", "client_id")
+INSERT INTO "projects"("project_id", "name", "started_at", "delivery_at", "delivered_at", "link_drive", "difficulty", "revenue", "price", "client_id")
 VALUES
   (
+    1,
     'Tá Soda', 
     '2019-07-06', 
     null, 
@@ -11,6 +12,7 @@ VALUES
     3, 1500, 1600, 1
   ),
   (
+    2,
     'Team Builder', 
     '2018-06-06', 
     null, 
@@ -19,6 +21,7 @@ VALUES
     2, 0, 0, 2
   ),
   (
+    3,
     'TechPet', 
     '2018-06-06', 
     null, 
@@ -27,6 +30,7 @@ VALUES
     2, 1500, 1500, 3
   ),
   (
+    4,
     'Dionísio', 
     '2019-09-20', 
     null, 
@@ -35,6 +39,7 @@ VALUES
     5, 180000, 180000, 7
   ),
   (
+    5,
     'Fada do Dente', 
     '2019-03-01', 
     '2019-06-01', 
@@ -43,6 +48,7 @@ VALUES
     3, 40000, 40000, 6
   ),
   (
+    6,
     'Bambu Quântico', 
     '2017-01-20', 
     null, 
@@ -51,6 +57,7 @@ VALUES
     3, 10, 10, 9
   ),
   (
+    7,
     'Dona Florina', 
     '2019-04-20', 
     null, 
@@ -59,6 +66,7 @@ VALUES
     3, 3500, 3500, 8
   ),
   (
+    8,
     'Silo', 
     '2019-07-20', 
     null, 
@@ -67,6 +75,8 @@ VALUES
     4, 7500, 7500, 7
   ),
   (
+
+    9,
     'Telemetria', 
     '2019-03-10', 
     '2019-05-28', 
@@ -75,6 +85,7 @@ VALUES
     2, 0, 0, 11
   ),
   (
+    10,
     'RiHappy', 
     '2019-01-10', 
     '2019-02-05', 
@@ -83,6 +94,7 @@ VALUES
     2, 30000, 30000, 10
   ),
   (
+    11,
     'BitContainer', 
     '2017-01-10', 
     '2018-02-05', 
