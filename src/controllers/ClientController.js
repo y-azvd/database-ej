92a4eecf9e2a3f2f5111c8a44b058179552b3bc9
Yudi Yamane <yudiyamane@gmail.com>
2019-11-16 13:21:10 -0300

    update client done and add dotenv.config() to database/inedx.js

--- src/controllers/ClientController.js
+++ src/controllers/ClientController.js
@@ -54,13 +54,6 @@ const ClientController = {
   },
 
   async update(request, response) {
-    /**
-     * - checar se membro existe
-     * - checar se email já é utlizado
-     * - sobrescrever infos
-     * - apagar todos os telefones do cliente
-     * - escrever os novos
-     */
     let results = await db.query(
       `SELECT * FROM clients
       WHERE client_id = '${request.params.id}'`
@@ -71,23 +64,64 @@ const ClientController = {
     }
 
     const client = results.rows[0]
-
     console.log(client)
+
+    /**
+     * checar se o email já está sendo usado
+     */
     results = await db.query(
       `SELECT email FROM clients
       WHERE email = '${request.body.email}'`
     )
 
-    // apagar telefones do cliente
-
     if (results.rows[0]) {
       return response.status(400).json({error: 'Email already in use'})
     }
 
+    /**
+     * Se deu tudo certo até aqui, atualize as informações
+     * do membro.
+     */
+    await db.query(
+      `UPDATE clients
+      SET
+        name = '${request.body.name}',
+        email = '${request.body.email}'
+      WHERE
+        client_id = '${client.client_id}';`
+    )
+
+    /**
+     * Apagar telefones e escrever os novos
+     */
+    if (request.body.phones.length > 0) {
+      await db.query(
+        `DELETE FROM client_phones
+        WHERE client_id = '${client.client_id}';`
+      )
+
+      let i;
+      let phonesQuery = `INSERT INTO "client_phones" ("client_id", "phone") VALUES\n`
+      let phones = request.body.phones
+
+      for (i = 0; i < phones.length-1; i++) {
+        phonesQuery += (`(${client.client_id},'${phones[i]}'),\n`)        
+      }
+
+      phonesQuery += (`('${client.client_id}','${phones[i]}') `)
+      phonesQuery += 'RETURNING phone;'
+      results = await db.query(phonesQuery)
+    }
+    
+    /**
+     * Enxertar telefones no cliente
+     */
     client.phones = results.rows.map(obj => obj.phone)
     
-    console.log(results.rows)
-    
+    /**
+     * O retorno nem sempre vai ter os números de telefone.
+     * Ainda não sei consertar isso.
+     */
     return response.json(client)
   }
 }
--- src/database/index.js
+++ src/database/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const { Pool } = require('pg')
 
 const connectionString = `postgres://${process.env.PSQL_USERNAME}:${process.env.PSQL_PASSWORD}@localhost:5432/mecajun`
